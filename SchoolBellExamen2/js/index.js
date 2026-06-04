ef310301db2a98c01ba1140f42ce6638a16283b4
Adria Navarro Savall <s.adria.navarro@gmail.com>
2026-06-04 14:33:04 +0200

    Checkvaliditi echo

--- SchoolBellExamen2/js/index.js
+++ SchoolBellExamen2/js/index.js
@@ -1,18 +1,20 @@
-document.addEventListener("DOMContentLoaded",main);
+document.addEventListener("DOMContentLoaded", main);
 
-async function main(){
+async function main() {
     await extraerDatos();
+
+    document.getElementById("enviar").addEventListener("click", validar, false);
 
     pintarLocal();
 
 }
 
-async function extraerDatos(){
-    try{
+async function extraerDatos() {
+    try {
         const res = await fetch('./data/data.json');
 
-        
-        if(!res.ok){
+
+        if (!res.ok) {
             throw new error("Error en la extraccion de lo datos");
         };
 
@@ -20,17 +22,17 @@ async function extraerDatos(){
 
         console.log(data);
 
-        localStorage.setItem("data",JSON.stringify(data));
+        localStorage.setItem("data", JSON.stringify(data));
 
-    }catch(error){
+    } catch (error) {
         console.log('Error-consulta:', error.message);
     }
 }
 
-function pintarLocal(){
+function pintarLocal() {
     let dataLocal = JSON.parse(localStorage.getItem("data"));
     let dataSchedule = dataLocal.schedules
-    
+
 
     let horario = dataSchedule[0]
 
@@ -65,9 +67,11 @@ function pintarLocal(){
         const btnEliminar = document.createElement("button");
         const Nodebtn = document.createTextNode("Borrar");
         btnEliminar.classList.add("btn", "btn-outline-danger");
+        btnEliminar.addEventListener("click", console.log("Descomentar y probar"));
+
         btnEliminar.appendChild(Nodebtn);
         tdAccion.appendChild(btnEliminar);
-        
+
         tr.appendChild(tdID);
         tr.appendChild(tdNombre);
         tr.appendChild(tdHora);
@@ -78,10 +82,10 @@ function pintarLocal(){
         table.appendChild(tr);
     });
 
-    
+
 }
 
-function nombreCancion(id){
+function nombreCancion(id) {
     let dataLocal = JSON.parse(localStorage.getItem("data"));
 
     let playList = dataLocal.playlists[0].songs;
@@ -89,17 +93,114 @@ function nombreCancion(id){
     let nombreCancion
 
     playList.forEach(element => {
-        if(id === element.id){
+        if (id === element.id) {
             nombreCancion = element.name
         }
     });
 
-    
 
-    console.log("playList disponibles",nombreCancion)
+
+    console.log("playList disponibles", nombreCancion)
 
     return nombreCancion;
+}
+
+//  function borrarDades(event, idbtn){
+//
+//      event.preventDefault();
+//
+//     const id = Number(idbtn)
+//
+//      console.log(id);
+//  }
 
 
-    
+function validarNombre() {
+
+    let element = document.getElementById("timeName");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error(element, "Deus d'introduïr un nom.");
+        }
+        if (element.validity.patternMismatch) {
+            error(element, "El nom ha de tindre entre 2 i 50 caracters.");
+        }
+        return false;
+    }
+    return true;
+
+}
+
+function validarHora() {
+
+    let element = document.getElementById("timeHour");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error(element, "Deus d'introduïr una hora.");
+        }
+        if (element.validity.patternMismatch) {
+            error(element, "Error de patron");
+        }
+        return false;
+    }
+    return true;
+
+}
+
+function validarDuracion() {
+
+    let element = document.getElementById("timeDuration");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error(element, "Deus d'introduïr una duracio.");
+        }
+        if (element.validity.patternMismatch) {
+            error(element, "Error de patron");
+        }
+        return false;
+    }
+    return true;
+
+}
+
+function validarCancion() {
+
+    let element = document.getElementById("timeSongSelect");
+    if (!element.checkValidity()) {
+        if (element.validity.valueMissing) {
+            error(element, "Deus d'introduïr una cancion.");
+        }
+        if (element.validity.patternMismatch) {
+            error(element, "Error de patron");
+        }
+        return false;
+    }
+    return true;
+
+}
+
+function validar(e) {
+    esborrarError();
+    e.preventDefault();
+    if (validarNombre() && validarHora() && validarDuracion() && validarCancion()) {
+        document.getElementById("formulario-producto").requestSubmit();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function error(element, missatge) {
+    let miss = document.createTextNode(missatge);
+    document.getElementById("missatgeError").appendChild(miss);
+    element.classList.add("text-danger");
+    element.focus();
+}
+
+function esborrarError() {
+    document.getElementById("missatgeError").textContent = "";
+    let formulari = document.forms[0];
+    for (let i = 0; i < formulari.elements.length; i++) {
+        formulari.elements[i].classList.remove("text-danger");
+    }
 }
